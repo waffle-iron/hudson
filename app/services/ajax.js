056ca17aa6c651815134fefd052d7bdd2e4c65a8
yashwin <yashwinpoojary@gmail.com>
2017-03-15 16:09:08 +0530

    enhance: add ajax service

---
+++ app/services/ajax.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+import AjaxService from 'ember-ajax/services/ajax';
+import ENV from 'hudson/config/environment';
+
+var HudsonAjaxService;
+
+HudsonAjaxService = AjaxService.extend({
+  host: ENV.host,
+  namespace: ENV.namespace,
+  session: Ember.inject.service(),
+  headers: Ember.computed('session.data.authenticated.b64token', {
+    get() {
+      var token;
+      token = this.get('session.data.authenticated.b64token');
+      return {
+        'Authorization': "Basic " + token,
+        'X-Product': ENV.product
+      };
+    }
+  })
+});
+
+export default HudsonAjaxService;
