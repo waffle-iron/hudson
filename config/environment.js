f234355e1b98cff9c06c47aa3d43a9b930036e74
yashwin <yashwinpoojary@gmail.com>
2017-03-28 12:59:51 +0530

    enhance: add endpoints

--- config/environment.js
+++ config/environment.js
@@ -40,7 +40,8 @@ module.exports = function(environment) {
       routeIfAlreadyAuthenticated: 'authenticated.index'
     },
     endpoints: {
-      changePassword: 'change_password'
+      changePassword: 'change_password',
+      subscription: 'subscriptions'
     }
   };
 
